0c346d328ca12d6f2e07fbc8a0abb85f797fc783
Fluxusfrequency <bennlewis@gmail.com>
2013-08-19 09:32:28 -0600

    Refactored runtime module, added instance variables as needed, changed quit command, added rescue clauses to encrypt and decrypt methods.

--- lib/encryptor/decrypt.rb
+++ lib/encryptor/decrypt.rb
@@ -6,20 +6,28 @@ module Encryptor
     end
 
     def decrypt_letter (letter, rotation1, rotation2, rotation3)
-      cipher_for_rotation1 = @cipher.cipher(-rotation1)
-      cipher_for_rotation1[letter]
-      cipher_for_rotation2 = @cipher.cipher(-rotation2)
-      cipher_for_rotation2[letter]
-      cipher_for_rotation3 = @cipher.cipher(-rotation3)
-      cipher_for_rotation3[letter]
+      begin
+        cipher_for_rotation1 = @cipher.cipher(-rotation1)
+        cipher_for_rotation1[letter]
+        cipher_for_rotation2 = @cipher.cipher(-rotation2)
+        cipher_for_rotation2[letter]
+        cipher_for_rotation3 = @cipher.cipher(-rotation3)
+        cipher_for_rotation3[letter]
+      rescue
+        puts "There was a problem decrypting this message."
+      end
     end
 
     def decrypt(string, rotation1 = @last_rotation1, rotation2 = @last_rotation2, rotation3 = @last_rotation3)
-      letters = string.split("")
-      results = letters.collect do |letter|
-        decrypt_letter(letter, rotation1, rotation2, rotation3)
+      begin
+        letters = string.split("")
+        results = letters.collect do |letter|
+          decrypt_letter(letter, rotation1, rotation2, rotation3)
+        end
+        results.join
+      rescue
+        puts "There was a problem decrypting this message."
       end
-      results.join
     end
 
     def decrypt_file(filename, rotation1, rotation2, rotation3)
--- lib/encryptor/encrypt.rb
+++ lib/encryptor/encrypt.rb
@@ -6,20 +6,28 @@ module Encryptor
     end
 
     def encrypt_letter (letter, rotation1, rotation2, rotation3)
-      cipher_for_rotation1 = @cipher.cipher(rotation1)
-      cipher_for_rotation1[letter]
-      cipher_for_rotation2 = @cipher.cipher(rotation2)
-      cipher_for_rotation2[letter]
-      cipher_for_rotation3 = @cipher.cipher(rotation3)
-      cipher_for_rotation3[letter]
+      begin
+        cipher_for_rotation1 = @cipher.cipher(rotation1)
+        cipher_for_rotation1[letter]
+        cipher_for_rotation2 = @cipher.cipher(rotation2)
+        cipher_for_rotation2[letter]
+        cipher_for_rotation3 = @cipher.cipher(rotation3)
+        cipher_for_rotation3[letter]
+      rescue
+        puts "There was a problem encrypting this message."
+      end
     end
 
     def encrypt(string, rotation1, rotation2, rotation3)
-      letters = string.split("")
-      results = letters.collect do |letter|
-        encrypt_letter(letter, rotation1, rotation2, rotation3)
+      begin
+        letters = string.split("")
+        results = letters.collect do |letter|
+          encrypt_letter(letter, rotation1, rotation2, rotation3)
+        end
+        results.join
+      rescue
+        puts "There was a problem encrypting this message."
       end
-      results.join
     end
 
     def encrypt_file(filename, rotation1, rotation2, rotation3)
